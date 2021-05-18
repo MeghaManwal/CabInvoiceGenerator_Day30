1bfccb044f80b10c00e5e9bbe30d3bda3b7a985f
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-18 20:04:02 +0530

    Added:To calculate the total Fare for a Cab ride

---
+++ CabInvoiceGeneratorService/src/test/java/com/cabservice/InvoiceServices_Test.java
@@ -0,0 +1,26 @@
+package com.cabservice;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class InvoiceServices_Test {
+	
+	@Test
+	public void givenDistanceAndMinute_shouldReturn_totalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 20.0;
+		int minute = 15;
+		double fare = invoiceGenerator.calculateFare(distance, minute);
+		assertEquals(215, fare,  0.0);	
+	}
+	
+	@Test
+	public void givenDistanceAndMinute_shouldReturn_minFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.3;
+		int minute = 2;
+		double fare = invoiceGenerator.calculateFare(distance, minute);
+		assertEquals(5, fare,  0.0);	
+	}
+
+}
